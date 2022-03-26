5429adaa6b9c909e665fda2a603279db2af3eb7b
mckay-moulton <79293626+mckay-moulton@users.noreply.github.com>
2022-03-26 11:57:02 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission13.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BowlingDbContext _context { get; set; }
+        //Contructor
+        public HomeController(BowlingDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var blah = _context.Bowlers.ToList();
+            return View(blah);
+        }
+    }
+}
---
+++ Models/BowlingDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Models
+{
+
+    public class BowlingDbContext : DbContext
+    {    //inherit from base options
+        public BowlingDbContext(DbContextOptions<BowlingDbContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Bowler> Bowlers { get; set; }
+    }
+}
